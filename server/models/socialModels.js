c12588c9f03d1d11aecd71f8737f06aa30d6e130
ryanmcd <rp.mcdan@gmail.com>
2021-03-09 15:57:27 -0500

    initial push, changed formatting and added comments to old code

--- server/models/socialModels.js
+++ server/models/socialModels.js
@@ -1,7 +1,8 @@
 const { Pool } = require('pg');
 
-const PG_URI =
-  'postgres://yvigeiaa:Qpe4Y1GB8ZP-Rg-fKaLfbYM4Lin6Ce7J@ziggy.db.elephantsql.com:5432/yvigeiaa';
+// const PG_URI = 'postgres://wnfdaaen:3cBUwRC0Jl16p-RzWFw0c5LNKCT92ryr@ziggy.db.elephantsql.com:5432/wnfdaaen';
+
+const PG_URI = 'postgres://yvigeiaa:Qpe4Y1GB8ZP-Rg-fKaLfbYM4Lin6Ce7J@ziggy.db.elephantsql.com:5432/yvigeiaa';
 
 const pool = new Pool({
   connectionString: PG_URI,
