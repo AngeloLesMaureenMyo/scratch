8957566c86199a67ddbac04ae4aba721328b1302
unknownbreaker <6026454+unknownbreaker@users.noreply.github.com>
2021-03-09 22:15:57 -0500

    Create alias generation controller

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,1 +1,15 @@
+const { adverbs, adjectives, animals } = require('../utils/aliasWords');
 const userController = {};
+
+userController.createAlias = (req, res, next) => {
+  const randIndex = (arrayLength) => {
+    return Math.floor(Math.random() * arrayLength);
+  };
+
+  const adverb = adverbs[randIndex(adverbs.length)];
+  const adjective = adjectives[randIndex(adjectives.length)];
+  const animal = animals[randIndex(animals.length)];
+
+  res.locals.alias = `${adverb} ${adjective} ${animal}`;
+  return next();
+};
