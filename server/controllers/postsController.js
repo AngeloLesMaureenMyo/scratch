981030dd800aeef1e515ec8696b3521f03b24504
Carthanial <ccarney51@gmail.com>
2021-03-10 13:47:51 -0500

    Handling req for all posts and thread posts
    	Added route to '/thread-posts' in routes/posts.js to handle get request send when user clicks the comment button on a top level post.
    	Added the postsController.getThreadPosts middleware to postsController.js for the above mentioned route.
    	Added error handling for the above mentioned middleware as well as pre-existing middleware getAllPosts.
    	Changed postsController.getAllPosts middleware to account for our new posts table schema in the db.
    		---> Will now get all posts whose parent_id is 0 meaning that post is the top level post.

--- server/controllers/postsController.js
+++ server/controllers/postsController.js
@@ -4,23 +4,43 @@ const postsController = {};
 
 postsController.getAllPosts = (req, res, next) => {
   const query = `
-    SELECT * FROM posts
-    ORDER BY _id DESC`;
+    SELECT * FROM posts p
+    WHERE p.parent_id = 0`;
   db.query(query).then((data) => {
     res.locals.allPosts = data.rows;
     return next();
   });
+  return next({
+    log: 'error at postController.getAllPosts',
+    status: 401,
+    message: 'failed to retrieve posts',
+  })
 };
-
+/*  Called on line 20 of routes/posts.js
+       -->Gets all child comments on a post  */
+postsController.getThreadPosts = (req, res, next) => {
+  const query = `
+  SELECT * FROM posts p
+  WHERE p.parent_id = ${req.body.postId}`;
+  db.query(query).then((data) => {
+    res.locals.threadPosts = data.rows;
+    return next()
+  })
+  return next({
+    log: 'error at postController.getThreadPosts' ,
+    status: 401,
+    message: 'failed to retrieve thread posts',
+  });
+};
 postsController.createPost = (req, res, next) => {
-  const { user_id, title, body } = req.body;
+  const { user_id, alias, body } = req.body;
 
   const query = `
-        INSERT INTO posts(user_id, title, body)
+        INSERT INTO posts(user_id, alias, body)
         VALUES($1, $2, $3)
         RETURNING *`;
 
-  db.query(query, [user_id, title, body]).then((data) => {
+  db.query(query, [user_id, alias, body]).then((data) => {
     res.locals.newPost = data.rows[0];
     return next();
   });
