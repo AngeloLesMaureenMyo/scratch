bf00067e446197c526671cb845e33126bf16286b
Kushal Talele <kushal.talele@gmail.com>
2021-03-09 19:50:43 -0500

    Update socialModels.js

--- server/models/socialModels.js
+++ server/models/socialModels.js
@@ -5,7 +5,7 @@ const { Pool } = require('pg');
 
 // original group's DB
 // const PG_URI = 'postgres://yvigeiaa:Qpe4Y1GB8ZP-Rg-fKaLfbYM4Lin6Ce7J@ziggy.db.elephantsql.com:5432/yvigeiaa';
-=======
+
 // const PG_URI = 'postgres://wnfdaaen:3cBUwRC0Jl16p-RzWFw0c5LNKCT92ryr@ziggy.db.elephantsql.com:5432/wnfdaaen';
 //postgres://wnfdaaen:3cBUwRC0Jl16p-RzWFw0c5LNKCT92ryr@ziggy.db.elephantsql.com:5432/wnfdaaen
 //working
