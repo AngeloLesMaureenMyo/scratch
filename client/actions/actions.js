3bb9443e569e85343f6013061298dce811bdc5e3
Kushal Talele <kushal.talele@gmail.com>
2021-03-09 21:28:37 -0500

    more updates

--- client/actions/actions.js
+++ client/actions/actions.js
@@ -13,28 +13,6 @@ export const getPosts = () => (dispatch) => {
       dispatch({ type: types.GET_POSTS, payload: data });
     });
 };
-export const upVote = (votes, postId, userId) => (dispatch) => {
-  //post request to backend
-  console.log('upvote fired', votes, postId, userId)
-  const reqBody = {
-    votes: votes + 1,
-    postId: postId,
-    userId: userId
-  }
-  //endpoint TBD
-  fetch('/posts', {
-    method: 'POST',
-    headers: { 'Content-Type': 'Application/JSON' },
-    body: JSON.stringify(reqBody),
-  })
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-      dispatch({ type: types.UPVOTE, payload: data });
-    })
-    .catch((e) => console.log(e));
-};
-  //body of post request to include id of current post and id of post creator
 
 export const savePost = (title, body, id) => (dispatch) => {
   const reqBody = {
@@ -65,3 +43,48 @@ export const updateBody = (newBody) => ({
   type: types.UPDATE_BODY,
   payload: newBody,
 });
+
+export const upVote = (votes, postId, userId) => (dispatch) => {
+  //post request to backend
+  console.log('upvote fired', votes, postId, userId)
+  const reqBody = {
+    votes: votes + 1,
+    postId: postId,
+    userId: userId
+  };
+  //endpoint TBD
+  fetch('/upvote', {
+    method: 'POST',
+    headers: { 'Content-Type': 'Application/JSON' },
+    body: JSON.stringify(reqBody),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      dispatch({ type: types.UPVOTE, payload: data });
+    })
+    .catch((e) => console.log(e));
+};
+  //body of post request to include id of current post and id of post creator
+export const downVote = (votes, postId, userId) => (dispatch) => {
+  //post request to backend
+  console.log('downvote fired', votes, postId, userId)
+  const reqBody = {
+    votes: votes + 1,
+    postId: postId,
+    userId: userId
+  };
+  //endpoint TBD
+  fetch('/downvote', {
+    method: 'POST',
+    headers: { 'Content-Type': 'Application/JSON' },
+    body: JSON.stringify(reqBody),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      dispatch({ type: types.UPVOTE, payload: data });
+    })
+    .catch((e) => console.log(e));
+};
+  //body of post request to include id of current post and id of post creator  
