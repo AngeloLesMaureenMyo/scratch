77eae8d95e8fdbbaa5d3dc9ac17e0ba24cd6144b
johnli <jli159@binghamton.edu>
2021-03-10 20:31:17 -0500

    Added description for 5 hour timezone difference adjustment

--- client/components/Post.jsx
+++ client/components/Post.jsx
@@ -3,10 +3,12 @@ import React from 'react';
 const timestamp = (psqlDate) => {
   const postTime = new Date(psqlDate).getTime()
   const currentTime = new Date().getTime()
+
+  // 300 minutes is added to reconcile the 5 hour timezone difference resulting from PSQL's timestamp without timezone format 
   let differenceInMin = (currentTime-postTime)/1000/60+300
-  console.log('postTime:', postTime)
-  console.log('currentTime:', currentTime)
-  console.log('differenceInMin:', differenceInMin)  
+  // console.log('postTime:', postTime)
+  // console.log('currentTime:', currentTime)
+  // console.log('differenceInMin:', differenceInMin)  
   if (differenceInMin < 1) {
     return "1m"
   } else if (differenceInMin < 60) {
