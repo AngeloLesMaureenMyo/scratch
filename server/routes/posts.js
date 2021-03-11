2c41935fc1d483485a1295ea86304fd1e8e17048
Carthanial <ccarney51@gmail.com>
2021-03-11 11:15:23 -0500

    getThreadPosts bugfix

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -22,11 +22,23 @@ router.get(
   }
 );
 /*  Gets all child posts to fill thread container when ---> onClick of button on post component  */
-router.get('/threads', authController.verifyUser, 
-postsController.getThreadPosts, 
-(req, res) => {
-  console.log('getting thread posts');
-  res.status(200).json(res.locals.threadPosts);
-});
+router.get('/threads',
+  authController.verifyUser, 
+  postsController.getThreadPosts, 
+  (req, res) => {
+    console.log('getting thread posts');
+    res.status(200).json(res.locals.threadPosts);
+  }
+);
+/* ******************************************** */
+/*  All routers below will be for posts karma  */
+router.post('/karma',
+  authController.verifyUser, 
+  postsController.updatePostKarma,
+  (req, res) => {
+    console.log('You voted on a post')
+    res.status(200).json(res.locals.newKarma)
+  }
+);
 
 module.exports = router;
